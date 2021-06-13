669d003fd228ca5b581fff2d6061f92d8f4c5252
lucasmoskun <contact@lucasmoskun.com>
2021-06-13 15:40:36 +0100

    initial test to to verify rhat using onboard web3 injection

--- src/components/oboard.js
+++ src/components/oboard.js
@@ -1,6 +1,7 @@
 import Onboard from 'bnc-onboard'
 import Web3 from 'web3'
-import {API_KEY} from './private/onboard_api'
+import {API_KEY} from '../private/onboard_api'
+import {ERC20_ABI} from './abi';
 
 let web3;
 
@@ -13,3 +14,15 @@ export const onboard = Onboard({
     }
   }
 });
+
+export const VerifyRHat = async () => {
+    var accounts = await web3.eth.getAccounts();
+    console.log("Accounts: " + accounts);
+    let account;
+    account = accounts[0];
+    const rHatAddress = "0x4f0fe57066ab1c84569dc6dd2edfe08b92f97f33";
+    const rHatContract = new web3.eth.Contract(ERC20_ABI, rHatAddress);
+    const balance = await rHatContract.methods.balanceOf(account).call();
+    console.log("RHAT Balance: " + balance);
+    console.log(typeof balance);
+}
--- src/rhat_club.js
+++ src/rhat_club.js
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
-import {onboard} from './components/oboard'
+import {onboard, VerifyRHat} from './components/oboard'
 
 function RHAT_CLUB() {
 
     useEffect(() => {
 
         const ConnectWallet = async () => {
-            await onboard.walletSelect();
-            await onboard.walletCheck();
+            //var walletCheckResult = await onboard.walletCheck();
+            //const wallet = onboard;
+            const selected = await onboard.walletSelect();
+            if(selected)
+            {
+                await onboard.walletCheck();
+                await VerifyRHat();
+            }
         };
         ConnectWallet();
 
